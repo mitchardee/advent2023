7357df75986802a091267300cc2a85c3f9405f8a
Mitch Hardee <emh3fm@virginia.edu>
2023-12-05 23:36:56 -0500

    Adding a bad solution.

--- 05/05-2.py
+++ 05/05-2.py
@@ -1,3 +1,6 @@
+from math import inf
+
+
 lines = open("input.txt").readlines()
 
 seeds = [int(n) for n in lines[0].split(": ")[1].split()]
@@ -30,14 +33,35 @@ def reverse_map_interval(interval):
 def inter_overlap(i1, i2):
     return 0 < i2[0] - i1[0] < i1[1] or 0 < i1[0] - i2[0] < i2[1]
 
-test_interval = (0, 1000000000)
+test_interval = (0, inf)
 while(True):
     rm = reverse_map_interval(test_interval)
     l = list(filter(lambda interval: inter_overlap(rm, interval), seed_intervals))
     if l:
         print(test_interval, rm, l)
+        print("Answer: ", test_interval[0])
         break
     else:
-        test_interval = (test_interval[0] + rm[1], 1000000000)
+        # print(test_interval, rm)
+        test_interval = (test_interval[0] + rm[1], inf)
 
-        
+# A version that doesn't use intervals runs in under a minute on the input
+# def reverse_map_val(val):
+#     for map in maps[::-1]:
+#         f = list(filter(lambda m: m[0] <= val < m[0]+m[2], map))
+#         if f:
+#             val += f[0][1] - f[0][0]
+#     return val
+
+# def val_overlap(val, interval):
+#     return 0 < val - interval[0] < interval[1]
+
+# v = 0
+# while(True):
+#     rm = reverse_map_val(v)
+#     l = list(filter(lambda interval: val_overlap_overlap(rm, interval), seed_intervals))
+#     if l:
+#         print(v, rm, l)
+#         break
+#     else:
+#         v += 1
