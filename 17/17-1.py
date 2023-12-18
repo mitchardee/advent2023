d25b0317abba29a6491b89a55a793da099c06307
Mitch Hardee <emh3fm@virginia.edu>
2023-12-18 01:06:37 -0500

    Day 17.

---
+++ 17/17-1.py
@@ -0,0 +1,41 @@
+from cmath import inf
+
+lines = open("sample.txt").readlines()
+for i in range(len(lines)):
+    lines[i] = [int(c) for c in lines[i].strip()]
+
+r = len(lines)
+c = len(lines[0])
+dirs = ((1, 0), (0, 1), (-1, 0), (0, -1))
+min_times_v = [[inf]*c for _ in range(r)]
+min_times_h = [[inf]*c for _ in range(r)]
+
+# walkers are (row, col, total_cooling, dir)
+walkers = [(0,0,0,0), (0,0,0,1)]
+
+while walkers:
+    walker = walkers.pop(0)
+
+    for d in (-1, 1):
+        dir = (walker[3]+d)%4
+        cooling = walker[2]
+        for t in range(1, 4):
+            x, y = walker[0]+t*dirs[dir][0], walker[1]+t*dirs[dir][1]
+
+            if (not (0 <= x < r and 0 <= y < c)):
+                break
+            
+            cooling += lines[x][y]
+
+            if dir%2 == 0:
+                if cooling < min_times_v[x][y]:
+                    min_times_v[x][y] = cooling
+                    walkers.append((x,y,cooling,dir))
+
+            else:
+                if cooling < min_times_h[x][y]:
+                    min_times_h[x][y] = cooling
+                    walkers.append((x,y,cooling,dir))
+
+
+print(min(min_times_h[-1][-1], min_times_v[-1][-1]))
